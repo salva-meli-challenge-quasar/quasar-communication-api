18c9dbcb21d3d7cdb316101804730a2a692d5564
ricardo <rsalva@devartis.com>
2021-01-19 13:48:44 -0300

    Adding boilerplate

---
+++ src/main/java/com/quasar/comunication/api/QuasarComunicationApiApplication.java
@@ -0,0 +1,13 @@
+package com.quasar.comunication.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuasarComunicationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuasarComunicationApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quasar/comunication/api/QuasarComunicationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quasar.comunication.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuasarComunicationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
